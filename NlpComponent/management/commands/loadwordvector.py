908f50ba1984f4c0a452bb95ec936cb52a7bdb66
shesl-meow@Manjaro <shesl-meow@qq.com>
2019-04-04 22:42:40 +0800

    [update] 更改插入数据库方式 多线程，最多 1w 线程

--- NlpComponent/management/commands/loadwordvector.py
+++ NlpComponent/management/commands/loadwordvector.py
@@ -5,13 +5,24 @@ from django.db import IntegrityError
 
 from NlpComponent.models import WordVector
 
+max_thread_amount = threading.Semaphore(10000)
 
-def insert_word(l):
-    try:
-        WordVector.objects.create(word=l[0], vector=json.dumps(l[1:]).replace(' ', ''))
-        return True
-    except IntegrityError:
-        return False
+
+class InsertThread(threading.Thread):
+    def __init__(self, l, line_num):
+        threading.Thread.__init__(self)
+        self.l = l
+        self.line_num = line_num
+
+    def run(self):
+        with max_thread_amount:
+            try:
+                WordVector.objects.create(word=self.l[0], vector=json.dumps(self.l[1:]).replace(' ', ''))
+                print(f"success: {str(self.line_num)}")
+                return True
+            except Exception as e:
+                print(f"fail: {str(self.line_num)} [{e}]")
+                return False
 
 
 class Command(BaseCommand):
@@ -28,9 +39,8 @@ class Command(BaseCommand):
 
         for line in f:
             l = line.split(' ')
-            t = threading.Thread(target=insert_word, args=(l,))
+            t = InsertThread(l, line_num)
             t.start()
             line_num += 1
-            print(str(line_num))
 
         f.close()
